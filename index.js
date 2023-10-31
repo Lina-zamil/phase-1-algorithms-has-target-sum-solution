befa04fda32c2124643222a9b7b0349cb8d507cb
lina abumahfood <lina.abumahfood@gmail.com>
2023-10-31 14:52:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  // 1.iterate through the outer array for(let i =0;i<array.length-1;i++)
+  for (let i = 0; i < array.length - 1; i++) {
+    // 2.create a variable called `complemant` const complement=target-array[i]
+    const complement = target - array[i]
+    //3.iterate through the inner array for(let j =i+1;i<array.length;i++)
+    for (let j = i + 1; j < array.length; j++) {
+      // 4.if array[j]===complement return true 
+      if (array[j] === complement) return true
+    }
+  }
+  //5. else return false
+  return false
 }
 
 /* 
@@ -8,10 +20,19 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+create a function called  `hasTargetSum(array, target)
+1.iterate through the outer array for(let i =0;i<array.length-1;i++)
+2.create a variable called `complemant` const complement=target-arry[i]
+3.iterate through the inner array for(let j =i+1;i<array.length;i++)  
+4.if array[j]===complement return true 
+5.else return false
 */
 
 /*
   Add written explanation of your solution here
+ create a function called  `hasTargetSum(array, target)`that takes two arrguments `array` & `target`
+  if two numbers in the array equal the target return true 
+  else return false
 */
 
 // You can run `node index.js` to view these console logs
